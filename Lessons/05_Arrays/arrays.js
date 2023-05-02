03c62fb1bc738e5ac28f5b2d3f23854dcb361573
Asim Ehmed <rocco.borocco95@gmail.com>
2023-05-02 22:47:26 +0400

    Lesson 5 done

--- Lessons/05_Arrays/arrays.js
+++ Lessons/05_Arrays/arrays.js
@@ -71,3 +71,94 @@ let cities = ["Baku", "Ganja", "Sumgait"];
 console.log(cities.length);
 
 //Getting index an element in arr array
+
+let programmingLanguages = [
+  "JavaScript",
+  "Python",
+  "Java",
+  "C++",
+  "Ruby",
+  "PHP",
+  "Swift",
+];
+let index = programmingLanguages.indexOf("Swift");
+
+if (programmingLanguages == -1) {
+  console.log("Not found");
+} else {
+  console.log("Found at index " + index);
+}
+
+console.log(programmingLanguages.includes("PHP"));
+
+//Checking array
+
+let myArray = [1, 2, 3, 4];
+
+if (Array.isArray(myArray) == true) {
+  console.log("My array is a array");
+} else {
+  console.log("My array is not a array");
+}
+
+console.log(Array.isArray(myArray));
+
+//Converting array to string
+
+const names = ["Asabeneh", "Mathias", "Elias", "Brook"];
+let namesToString = names.toString();
+console.log(namesToString);
+
+//Joining array elements
+
+let myArray1 = ["JavaScript", "Python", "Java", "C++"];
+let myArray1Join = myArray.join(" | ");
+//we can pass different string parameter
+
+console.log(myArray1Join);
+
+//Slice out
+
+let myArray2 = ["JavaScript", "Python", "Java", "C++", "PHP"];
+console.log(myArray2.slice(1, 4));
+
+//Splice method in array
+
+let myArray3 = ["JavaScript", "Python", "Java", "C++", "PHP"];
+let deletedElements = myArray3.splice(2, 2, "Ruby", "Swift");
+
+console.log(myArray3); // ["JavaScript", "Python", "Ruby", "Swift", "PHP"]
+console.log(deletedElements); // ["Java", "C++"]
+
+//Adding item to an array using push
+
+let newArray = ["item1", "item2", "item3"];
+let newArrayAdd = newArray.push("item4");
+console.log(newArray);
+
+//Add an element from the beginning
+let addItemToBeginnig = newArray.unshift("item0");
+console.log(newArray);
+
+//Removing the end element using pop
+
+let myArray4 = ["JavaScript", "Python", "Java", "C++", "PHP"];
+let lastElement = myArray4.pop();
+
+console.log(myArray4); // ["JavaScript", "Python", "Java", "C++"]
+console.log(lastElement); // "PHP"
+
+//Removing an element from the beginning
+let firstElement = myArray4.shift();
+console.log(firstElement);
+console.log(myArray4);
+console.log(myArray4.reverse());
+console.log(myArray.sort());
+
+//Array of arrays
+
+const frontEnd = ["HTML", "CSS", "JS", "React", "Redux"];
+const backEnd = ["Node", "Express", "MongoDB"];
+const fullStack = [frontEnd, backEnd];
+console.log(fullStack);
+console.log(fullStack[0]);
